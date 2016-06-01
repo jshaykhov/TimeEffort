003166d4d9d1a56a1273a943246f41f51d7aa269
Nargizaaa <jshaykhov@wiut.uz>
2016-06-01 11:59:20 +0500

    ado net

--- TimeEffortCore/Services/UserService.cs
+++ TimeEffortCore/Services/UserService.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeEffortCore.Entities;
 
 namespace TimeEffortCore.Services
 {
-    public class UserDBService
+    public class UserService
     {
-         private static dbEntities db;
+        private static time_trackerEntities db;
         public UserService()
         {
-            db = new dbEntities();
+            db = new time_trackerEntities();
         }
     }
 }
