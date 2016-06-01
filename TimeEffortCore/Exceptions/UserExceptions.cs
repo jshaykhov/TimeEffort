5614bb2d22421db0370f4183d520efa3b25b5496
Nargizaaa <jshaykhov@wiut.uz>
2016-06-01 12:46:38 +0500

    ado net

---
+++ TimeEffortCore/Exceptions/UserExceptions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeEffortCore.Exceptions
+{
+     [Serializable]
+    public class NoUserNameException : Exception
+    {
+        public NoUserNameException()
+        {
+        }
+
+        public NoUserNameException(string message)
+        : base(message)
+        {
+        }
+
+        public NoUserNameException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+
+    [Serializable]
+    public class NoUserPasswordException : Exception
+    {
+        public NoUserPasswordException()
+        {
+        }
+
+        public NoUserPasswordException(string message)
+        : base(message)
+        {
+        }
+
+        public NoUserPasswordException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+    [Serializable]
+    public class NoUserEmailException : Exception
+    {
+        public NoUserEmailException()
+        {
+        }
+
+        public NoUserEmailException(string message)
+        : base(message)
+        {
+        }
+
+        public NoUserEmailException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+    [Serializable]
+    public class NoUserEmailException : Exception
+    {
+        public NoUserEmailException()
+        {
+        }
+
+        public NoUserEmailException(string message)
+            : base(message)
+        {
+        }
+
+        public NoUserEmailException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
