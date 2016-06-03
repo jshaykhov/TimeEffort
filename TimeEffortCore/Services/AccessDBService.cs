afe4e737d9713c40ed6cad9ace23b3e15bb8f91a
rafatdin <rafatdin@gmail.com>
2016-06-03 14:22:37 +0500

    helpers and project controller

---
+++ TimeEffortCore/Services/AccessDBService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeEffortCore.Entities;
+
+namespace TimeEffortCore.Services
+{
+    public class AccessDBService
+    {
+        private time_trackerEntities1 db;
+        public AccessDBService()
+        {
+            db = new time_trackerEntities1();
+        }
+
+        public bool IsInvolved(int userId, int projectId)
+        {
+            return db.Access.Any(x => x.UserID == userId && x.ProjectID == projectId);
+        }
+
+        public List<Project> GetProjectsByUser(string username)
+        {
+            List<Project> returningList = new List<Project>();
+
+            var user = db.UserInfo.FirstOrDefault(u => u.Username == username);
+            var projects = db.Project.ToList();
+            
+            
+            foreach (Project p in projects)
+            {
+                if (IsInvolved(user.ID, p.ID) && p.ManagerID != user.ID)
+                    returningList.Add(p);
+            }
+
+            return returningList;
+
+        }
+
+    }
+}
