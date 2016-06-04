000f33eb445ab584a7003d2da536c11afa3c3629
rafatdin <rafatdin@gmail.com>
2016-06-04 22:54:28 +0500

    a loooot

---
+++ TimeEffort/Controllers/MonitorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeEffort.Models;
+using TimeEffortCore.Services;
+
+namespace TimeEffort.Controllers
+{
+    public class MonitorController : Controller
+    {
+        //---------------Singleton------------------
+        public static WorkloadDbService _db;
+
+        public static WorkloadDbService db
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new WorkloadDbService();
+                return _db;
+            }
+        }
+        //---------------Singleton------------------
+
+        //
+        // GET: /Monitor/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public MonitorViewModel GetResult()
+        {
+            var result = new MonitorViewModel();
+
+            
+
+            return result;
+        }
+
+        private List<ProjectMontior> GetProjects(DateTime from, DateTime to, string? User)
+        {
+            var result = new List<ProjectMontior>();
+
+            var allProjects = db.GetAll().Where(x => x.Date >= from && x.Date <= to).ToList();
+            
+            
+
+            return result;
+        }
+	}
+}
---
+++ TimeEffort/Models/MonitorViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TimeEffortCore.Entities;
+
+namespace TimeEffort.Models
+{
+    public class MonitorViewModel
+    {
+        public ProjectMontior projects { get; set; }
+        public EmployeeMonitor employees { get; set; }
+        public QueryMonitor query { get; set; }
+    }
+
+    public class ProjectMontior
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Employee")]
+        public string Employee { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+        [DataType(DataType.Date)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Date")]
+        public string Date { get; set; }
+
+        [DataType(DataType.Duration)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Duration")]
+        public string Duration { get; set; }
+
+    }
+
+    public class EmployeeMonitor
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Project")]
+        public string Project { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+        [DataType(DataType.Date)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Date")]
+        public string Date { get; set; }
+
+        [DataType(DataType.Duration)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Duration")]
+        public string Duration { get; set; }
+    }
+
+    public class QueryMonitor
+    {
+        public UserInfo Employee { get; set; }
+        public Project Project { get; set; }
+
+        [DataType(DataType.Date)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Date)]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+    }
+
+
+}
