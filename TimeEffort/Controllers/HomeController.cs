58e804224d5288a1389d3c20d98a8d6cd1a0a47c
rafatdin <rafatdin@gmail.com>
2016-06-17 18:15:43 +0500

    changes lasst

--- TimeEffort/Controllers/HomeController.cs
+++ TimeEffort/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using TimeEffort.Helper;
 
@@ -13,6 +15,13 @@ namespace TimeEffort.Controllers
         
         public ActionResult Index()
         {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration(Request.ApplicationPath);
+            ConfigurationSection section = config.GetSection("connectionStrings");
+            if (!section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+                config.Save();
+            }
             //
             return View("Index" + HelperUser.GetRoleName(User), masterName: "~/Views/Shared/_Layout" + HelperUser.GetRoleName(User) + ".cshtml");
         }
