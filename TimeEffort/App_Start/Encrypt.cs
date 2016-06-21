57b449676f2c1f26529473e800d01d0d2877f68a
rafatdin <rafatdin@gmail.com>
2016-06-21 09:48:21 +0500

    Web config encryption and 404 page not found redirection

---
+++ TimeEffort/App_Start/Encrypt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace TimeEffort.App_Start
+{
+    public class Encrypt
+    {
+        public static void EncryptWebConfig(HttpRequestBase request)
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration(request.ApplicationPath);
+            ConfigurationSection section = config.GetSection("connectionStrings");
+            if (!section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+                config.Save();
+            }
+        }
+    }
+
+}
